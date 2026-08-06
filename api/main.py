c41d8c981104a7340c7a51597b89ebf1ccbf0b65
Praise Danjuma <futurehublabs@gmail.com>
2026-08-06 13:36:07 +0100

    FuElectric-AI Version 2.0 Milestone 1 - Diagnosis API working

--- api/main.py
+++ api/main.py
@@ -1,6 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+# from models.equipment import DiagnosisRequest
+# from data import HOME_APPLIANCE_FAULTS
 
-app  = FastAPI(
+app  = FastAPI( 
     title="FuElectric-AI",
     version="2.0",
     description="AI Equipment Diagnosis API",
@@ -18,3 +20,27 @@ def health():
         "status": "Running",
         "system": "FuElectric-AI"
     }
+@app.post("/diagnose")
+def diagnose():
+    return{"message":"diagnosis endpoint works"}
+
+    equipment = request.equipment.lower()
+    fault = request.fault.lower()
+
+    if equipment not in HOME_APPLIANCE_FAULTS:
+        raise HTTPException(status_code=404, detail="Equipment not found")
+
+    if fault not in HOME_APPLIANCE_FAULTS:
+        raise HTTPException(
+            status_code=404,
+            detail="Fault not found in knowledge base."
+              )
+
+    diagnosis = HOME_APPLIANCE_FAULTS[fault]
+
+    return {
+        "equipment": equipment,
+        "fault":  request.fault,
+        **diagnosis
+    }
+
